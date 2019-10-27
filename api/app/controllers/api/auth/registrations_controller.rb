7333f71725b49fa5fa87afcbab11f0ace47fa97e
norihito <norihito0626@gmail.com>
2019-10-27 23:52:49 +0900

    create devise token auth user model

---
+++ api/app/controllers/api/auth/registrations_controller.rb
@@ -0,0 +1,16 @@
+module Api
+  module Auth
+    class RegistrationsController < DeviseTokenAuth::RegistrationsController 
+
+    private
+      def sign_up_params
+        params.permit(:username, :email, :password, :password_confirmation)
+      end
+ 
+      def account_update_params
+        params.permit(:username, :email)
+      end
+    end
+  end
+end
+
