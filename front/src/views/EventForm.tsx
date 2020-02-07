238323c6b8fde2284f15285d6162f352eb1fe8ca
norihito <norihito0626@gmail.com>
2020-02-08 03:21:27 +0900

    Update EventForm

--- front/src/views/EventForm.tsx
+++ front/src/views/EventForm.tsx
@@ -23,8 +23,16 @@ interface EventState {
 class EventForm extends Component<EventProps, EventState> {
   constructor(props: EventProps) {
     super(props);
-    this.state = {startValue: '', startYear: '', endYear: '', intervalYear: '',
-                  interestRate: '', eventMemo: '', category_id: '', subcategory_id: ''};
+    this.state = {
+      startValue: "",
+      startYear: "35",
+      endYear: "",
+      intervalYear: "",
+      interestRate: "",
+      eventMemo: "",
+      category_id: "",
+      subcategory_id: ""
+    };
   }
 
   handleInputChange = (e: React.FormEvent<HTMLInputElement>): void => {
@@ -36,6 +44,9 @@ class EventForm extends Component<EventProps, EventState> {
       EventState,
       keyof EventState
     >);
+
+    // For Debug
+    console.log(this.state);
   };
 
   handleSave = (e: React.MouseEvent<HTMLButtonElement>): void => {
@@ -44,7 +55,7 @@ class EventForm extends Component<EventProps, EventState> {
     console.log(this.state);
     // this.props.saveEvent(this.state);
     this.handleBack();
-  }
+  };
 
   handleBack = (): void => {
     this.props.history.goBack();
@@ -71,39 +82,111 @@ class EventForm extends Component<EventProps, EventState> {
                 <Card>
                   <Card.Body>
                     <Form>
-                      <Form.Group controlId="formBasicStartValue">
-                        <Form.Label>収入</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="startValue"
-                          value={this.state.startValue}
-                          onChange={this.handleInputChange}
-                          placeholder="500(万円)"
-                        />
-                      </Form.Group>
                       <Form.Row>
-                        <Form.Group as={Col} controlId="formBasicStartYear">
-                          <Form.Label>開始</Form.Label>
+                        <Form.Group as={Col} controlId="formCategory">
+                          <Form.Label>大項目</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="category_id"
+                            value={this.state.category_id}
+                            onChange={this.handleInputChange}
+                          >
+                            <option>給与収入</option>
+                            <option>事業収入 </option>
+                            <option>不動産収入</option>
+                            <option>その他（固定収入）</option>
+                            <option>その他（一時収入）</option>
+                          </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formBasicSubCategory">
+                          <Form.Label>小項目</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="subcategory_id"
+                            value={this.state.subcategory_id}
+                            onChange={this.handleInputChange}
+                          >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Row>
+                        <Form.Group as={Col} controlId="formBasicStartValue">
+                          <Form.Label>収入</Form.Label>
                           <Form.Control
                             type="text"
-                            name="startYear"
-                            value={this.state.startYear}
+                            name="startValue"
+                            value={this.state.startValue}
                             onChange={this.handleInputChange}
+                            placeholder="500(万円)"
                           />
                         </Form.Group>
-                        <Form.Group as={Col} controlId="formBasicEndYear">
-                          <Form.Label>終了</Form.Label>
+                        <Form.Group as={Col} controlId="formBasicInterestRate">
+                          <Form.Label>変動率</Form.Label>
                           <Form.Control
                             type="text"
-                            name="endYear"
-                            value={this.state.endYear}
+                            name="interestRate"
+                            value={this.state.interestRate}
                             onChange={this.handleInputChange}
+                            placeholder="1(%)"
                           />
                         </Form.Group>
                       </Form.Row>
-                    <Button variant="primary" onClick={this.handleSave}>
-                      保存
-                    </Button>
+                      <Form.Row>
+                        <Form.Group as={Col} controlId="formBasicStartYear">
+                          <Form.Label>開始年齢</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="startYear"
+                            value={this.state.startYear}
+                            onChange={this.handleInputChange}
+                          >
+                            { [...Array(100)].map((_, i) => <option key={i}>{i}</option>) }
+                          </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formBasicEndYear">
+                          <Form.Label>終了年齢</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="endYear"
+                            value={this.state.endYear}
+                            onChange={this.handleInputChange}
+                          >
+                            { [...Array(100)].map((_, i) => (i > Number(this.state.startYear)) ? <option key={i}>{i}</option> : null ) }
+                          </Form.Control>
+                        </Form.Group>
+                        <Form.Group as={Col} controlId="formBasicInterval">
+                          <Form.Label>頻度</Form.Label>
+                          <Form.Control
+                            as="select"
+                            name="intervalYear"
+                            value={this.state.intervalYear}
+                            onChange={this.handleInputChange}
+                          >
+                            <option>毎年</option>
+                            <option>２年毎</option>
+                            <option>５年毎</option>
+                            <option>１０年毎</option>
+                          </Form.Control>
+                        </Form.Group>
+                      </Form.Row>
+                      <Form.Group>
+                        <Form.Label>メモ</Form.Label>
+                        <Form.Control
+                          as="textarea"
+                          rows="2"
+                          name="eventMemo"
+                          value={this.state.eventMemo}
+                          onChange={this.handleInputChange}
+                        />
+                      </Form.Group>
+                      <Button variant="primary" onClick={this.handleSave}>
+                        保存
+                      </Button>
                     </Form>
                   </Card.Body>
                 </Card>
