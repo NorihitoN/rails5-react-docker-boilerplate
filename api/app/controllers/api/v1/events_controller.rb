4b5d3e11bde62b1fa9f5024902198cc81d3b7260
norihito <norihito0626@gmail.com>
2020-02-12 00:06:49 +0900

    Event Controller and Byebug on Docker Setting

--- api/app/controllers/api/v1/events_controller.rb
+++ api/app/controllers/api/v1/events_controller.rb
@@ -3,9 +3,12 @@ module Api
     class EventsController < ApplicationController
     before_action :authenticate_api_user!
 
-    def index
-      events = current_member.events
+    # Need to refactoring to GET current_member for index, show, create, delete, update
+    # Need to refactoring to CREATE_EVENT_AND_INCOME/EXPENSE
 
+    def index
+      current_member = Member.find(params[:member_id])
+      events = current_member.events
       render json: {
         events: events,
         is_success: true
@@ -13,6 +16,7 @@ module Api
     end
 
     def show 
+      current_member = Member.find(params[:member_id])
       event = current_member.events.find(params[:id])
 
       render json: {
@@ -22,6 +26,7 @@ module Api
     end
 
     def create
+      current_member = Member.find(params[:member_id])
       event = current_member.events.build(event_params)
 
       if event.save
@@ -29,15 +34,17 @@ module Api
         start_value = event.start_value 
         category_id = event.category_id
         subcategory_id = event.subcategory_id
+        interval_year = event.interval_year
         interest_rate = event.interest_rate
         event_id = event.id
+        year_list = (event.start_year..event.end_year).select.each_with_index{ |_,i| i%interval_year==0 }
 
         if event.category.category_type == "income"
-          (event.start_year..end).map.with_index do |year, index|
-            current_member.incomes.create(income_value: start_value * (1*interest_rate)**index, event_id: event_id, category_id: category_id, income_year: year, subcategory_id: subcategory_id)
+          year_list.map.with_index do |year, index|
+            current_member.incomes.create(income_value: start_value * (1+interest_rate)**index, event_id: event_id, category_id: category_id, income_year: year, subcategory_id: subcategory_id)
           end
         elsif event.category.category_type == "expense"
-          (event.start_year..end).map.with_index do |year, index|
+          year_list.map.with_index do |year, index|
             current_member.expenses.create(expense_value: start_value * (1+interest_rate)**index, event_id: event_id,  category_id: category_id, expense_year: year, subcategory_id: subcategory_id)
           end
         end
@@ -47,20 +54,52 @@ module Api
         render json: { error: "Event was not saved properly." }, status: 404
       end
 
-      rescue => e
-        render json: { error: e.message, is_success: false }, status: 404
+    rescue => e
+      render json: { error: e.message, is_success: false }, status: 404
     end
 
     def update
-      event = current_api_user.event
+      current_member = Member.find(params[:member_id])
+      event = current_member.events.find(params[:id])
+      
+      if event.category.category_type == "income"
+        event.incomes.destroy_all
+      elsif event.category.category_type =="expense"
+        event.expenses.destroy_all
+      end
 
       if event.update(event_params)
+        
+        start_value = event.start_value 
+        category_id = event.category_id
+        subcategory_id = event.subcategory_id
+        interval_year = event.interval_year
+        interest_rate = event.interest_rate
+        event_id = event.id
+        year_list = (event.start_year..event.end_year).select.each_with_index{ |_,i| i%interval_year==0 }
+
+        if event.category.category_type == "income"
+          year_list.map.with_index do |year, index|
+            current_member.incomes.create(income_value: start_value * (1+interest_rate)**index, event_id: event_id, category_id: category_id, income_year: year, subcategory_id: subcategory_id)
+          end
+        elsif event.category.category_type == "expense"
+          year_list.map.with_index do |year, index|
+            current_member.expenses.create(expense_value: start_value * (1+interest_rate)**index, event_id: event_id,  category_id: category_id, expense_year: year, subcategory_id: subcategory_id)
+          end
+        end
+
         render json: { is_success: true }, status: :ok
+      else
+        render json: { error: "Event was not updated properly." }, status: 404
       end
+
+    rescue => e
+      render json: { error: e.message, is_success: false }, status: 404
     end
 
     def destroy
-      event = current_api_user.event
+      current_member = Member.find(params[:member_id])
+      event = current_member.events.find(params[:id])
 
       if event.destroy
         render json: { is_success: true }, status: :ok
@@ -74,8 +113,8 @@ module Api
         params.require(:event).permit(:start_value, :start_year, :end_year, :interval_year, :interest_rate, :event_memo, :category_id, :subcategory_id)
       end
 
-      def current_member
-        Member.where(member_id)
+      def set_current_member
+        current_member = Member.find(params[:member_id])
       end
 
     end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
 
       namespace 'v1' do
         resources :families
-        resources :members
-        resources :events
+        resources :members do
+          resources :events
+        end
       end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
